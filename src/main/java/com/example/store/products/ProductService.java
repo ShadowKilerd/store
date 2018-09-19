0105dc7537c4cba88f77acc256c2e59f6bf08a75
Hu Le <lhu@thoughtworks.com>
2018-09-19 16:18:17 +0800

    add product controller test

---
+++ src/main/java/com/example/store/products/ProductService.java
@@ -0,0 +1,15 @@
+package com.example.store.products;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<Product> getProducts() {
+        return new ArrayList<>();
+    }
+}
